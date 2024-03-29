10ae6b00dfe873a69ff31a3d6b012152d68dd52a
Bhatti-Sahab-007 <165411523+Bhatti-Sahab-007@users.noreply.github.com>
2024-03-29 21:57:48 +0500

    Update Bhatti.py

--- Bhatti.py
+++ Bhatti.py
@@ -1,1 +1,10 @@
-
+import os,platform
+os.system('git pull')
+os.system("clear")
+print('\033[92;1m Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/Hf6WUhpV822AmZlGM6RA8P')
+bhatti=platform.architecture()[0]
+if bhatti=="32bit":
+    __import__("p32")
+elif bhatti=="64bit":
+    __import__("p64")
